ac98b483397664d418bb7bb343de9eca67585714
SyntheticAutomation <mclampet@msudenver.edu>
2018-12-13 09:18:28 -0700

    Adds test for iteration_1

--- spec/features/user_can_create_account_spec.rb
+++ spec/features/user_can_create_account_spec.rb
@@ -2,11 +2,23 @@ require 'rails_helper'
 
 describe 'a visitor to our web app' do
   it 'can create an account' do
-    
+
     visit '/'
 
-    click_link "Create User"
+    click_link "Sign up for an account"
 
-    expect(current_path).to eq(new_user)
+    expect(current_path).to eq(new_user_path)
+
+    name = "AskYoGirlAboutMe"
+    email = "chrishansen@gmail.com"
+    password = "password"
+
+    fill_in :user_name, with: name
+    fill_in :user_email, with: email
+    fill_in :user_password, with: password
+
+    click_on "Create User"
+
+    expect(current_path).to eq(user_path("#{Book.last.id}"))
   end
 end
